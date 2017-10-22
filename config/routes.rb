6f313be0beb7b65ab31a9ca8f0f4b02dd5251e47
Nguyen Quoc Anh Tu <ngqanhtu@outlook.com>
2017-10-23 06:00:08 +0700

    Add messages controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   resources :users
+  resources :messages
   get 'welcome/index'
   root 'welcome#index'
 end
