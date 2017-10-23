6a335317bb0903bc5bbea53153e0ce4ed56e4e35
Trung <trungpq999@gmail.com>
2017-10-24 00:06:23 +0700

    create message

---
+++ app/controllers/message_controller.rb
@@ -0,0 +1,16 @@
+class MessageController < ApplicationController
+    def new  
+        @message = Message.new      
+    end
+
+    def create
+        @message = Message.new(message_params)
+        render 'new' 
+    end
+
+    private
+    def message_params
+        params.require(:messages).permit(:status,:sender_id,:receiver_id,:content,:sent_time,:seen_time)
+        
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   get '/signup', to: 'users#new'
 
+  get  '/message', to: 'message#new'
+  post '/message', to: 'message#create'
+
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
+
   delete '/logout',  to: 'sessions#destroy'
   resources :users
+  resources :messages
   get 'welcome/index'
   root 'welcome#index'
 end
